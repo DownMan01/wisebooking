30f548052c0846d1eeebf73ec41c184afad1c973
J <98541904+DownMan01@users.noreply.github.com>
2024-08-22 00:30:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-document.querySelectorAll('main img').forEach(img => {
-  img.style.setProperty('--delay', Math.random() + 's');
-  img.style.setProperty('--rotate', (Math.random() * 300 * (Math.random() > .5 ? 1 : -1)) + 'deg');
-  img.style.setProperty('--scale', Math.random());
+document.getElementById('animateButton').addEventListener('click', () => {
+  document.querySelectorAll('#imageContainer img').forEach(img => {
+    img.style.setProperty('--delay', Math.random() + 's');
+    img.style.setProperty('--rotate', (Math.random() * 300 * (Math.random() > .5 ? 1 : -1)) + 'deg');
+    img.style.setProperty('--scale', Math.random());
+    img.style.animation = 'float 2s var(--ease-spring-2) var(--delay) infinite';
+  });
 });
